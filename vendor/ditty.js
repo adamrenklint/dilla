11c0966205ae5674a0857b11cc40602dd3e695c0
Adam Renklint <adam@renklint.com>
2015-09-15 23:26:08 +0200

    break loops up into functions

--- vendor/ditty.js
+++ vendor/ditty.js
@@ -95,6 +95,63 @@ proto.write = function(obj){
   this._transform(obj)
 }
 
+proto._updateItemProperties = function(item, obj) {
+  var state = this._state
+  var from = obj.from
+  var to = obj.to
+  var time = obj.time
+  var nextTime = obj.time + obj.duration
+  var beatDuration = obj.beatDuration
+  var queue = state.queue
+
+  if (to > item.position || shouldSendImmediately(item, state.loops[item.id])){
+    if (to > item.position){
+      var delta = (item.position - from) * beatDuration
+      item.time = time + delta
+    } else {
+      item.time = time
+      item.position = from
+    }
+    queue.splice(i, 1)
+    this.push(item)
+  }
+};
+
+proto._queueEvent = function(event, id, localQueue, loopLength, obj) {
+  var from = obj.from
+  var to = obj.to
+  var time = obj.time
+  var beatDuration = obj.beatDuration
+  var startPosition = getAbsolutePosition(event[0], from, loopLength)
+  var endPosition = startPosition + event[1]
+
+  if (startPosition >= from && startPosition < to){
+
+    var delta = (startPosition - from) * beatDuration
+    var duration = event[1] * beatDuration
+    var startTime = time + delta
+    var endTime = startTime + duration
+
+    localQueue.push({
+      id: id,
+      event: 'start',
+      position: startPosition,
+      args: event[4],
+      time: startTime
+    })
+
+    if (duration) {
+      localQueue.push({
+        id: id,
+        event: 'stop',
+        position: endPosition,
+        args: event[4],
+        time: endTime
+      })
+    }
+  }
+}
+
 proto._transform = function(obj){
   var begin = window.performance.now()
   var endAt = begin + (obj.duration * 5000)
@@ -110,18 +167,7 @@ proto._transform = function(obj){
   var localQueue = []
 
   for (var i=queue.length-1;i>=0;i--){
-    var item = queue[i]
-    if (to > item.position || shouldSendImmediately(item, state.loops[item.id])){
-      if (to > item.position){
-        var delta = (item.position - from) * beatDuration
-        item.time = time + delta
-      } else {
-        item.time = time
-        item.position = from
-      }
-      queue.splice(i, 1)
-      this.push(item)
-    }
+    this._updateItemProperties(queue[i], obj);
   }
 
   for (var i=0;i<ids.length;i++){
@@ -131,45 +177,18 @@ proto._transform = function(obj){
     var loopLength = state.lengths[id]
 
     for (var j=0;j<events.length;j++){
-
-      var event = events[j]
-      var startPosition = getAbsolutePosition(event[0], from, loopLength)
-      var endPosition = startPosition + event[1]
-
-      if (startPosition >= from && startPosition < to){
-
-        var delta = (startPosition - from) * beatDuration
-        var duration = event[1] * beatDuration
-        var startTime = time + delta
-        var endTime = startTime + duration
-
-        localQueue.push({
-          id: id,
-          event: 'start',
-          position: startPosition,
-          args: event.slice(4),
-          time: startTime
-        })
-
-        localQueue.push({
-          id: id,
-          event: 'stop',
-          position: endPosition,
-          args: event.slice(4),
-          time: endTime
-        })
-      }
+      this._queueEvent(events[j], id, localQueue, loopLength, obj);
     }
   }
 
   // ensure events stream in time sequence
-  localQueue.sort(compare)
+  // localQueue.sort(compare)
   for (var i=0;i<localQueue.length;i++){
     var item = localQueue[i]
     if (item.time < nextTime){
-      if (window.performance.now() < endAt){
+      // if (window.performance.now() < endAt){
         this.push(item)
-      }
+      // }
     } else {
       // queue event for later
       queue.push(item)
