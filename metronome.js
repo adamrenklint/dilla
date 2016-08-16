0098485fb1d82013e83ecf1e3889f16cffdfc337
Adam Renklint <adam@renklint.com>
2016-08-16 10:00:27 +0200

    update example

--- metronome.js
+++ metronome.js
@@ -129,15 +129,19 @@ proto.getPositionFromClockPosition = function getPositionFromClockPosition (posi
   return (bars + 1) + '.' + (beats + 1) + '.' + ticks;
 };
 
-proto.getClockPositionFromPosition = memoize(function getClockPositionFromPosition (position) {
+proto.getClockPositionFromPosition = memoize(function getClockPositionFromPosition (position, beatsPerBar) {
   var parts = position.split('.');
   var bars = parseInt(parts[0], 10) - 1;
   var beats = parseInt(parts[1], 10) - 1;
   var ticks = parseInt(parts[2], 10) - 1;
-  return (bars * this._beatsPerBar) + beats + (ticks / 96);
+  return (bars * beatsPerBar) + beats + (ticks / 96);
 });
 
-proto.getPositionWithOffset = memoize(function getPositionWithOffset (position, offset) {
+proto.getPositionWithOffset = function getPositionWithOffset (position, offset) {
+  return this.getPositionWithOffsetAndBeatsPerBar(position, offset, this.beatsPerBar());
+};
+
+proto.getPositionWithOffsetAndBeatsPerBar = memoize(function getPositionWithOffsetAndBeatsPerBar (position, offset, beatsPerBar) {
   if (!checkValid.positionString(position)) {
     throw new Error('Invalid argument: position is not a valid position string');
   }
@@ -147,7 +151,7 @@ proto.getPositionWithOffset = memoize(function getPositionWithOffset (position, 
   if (!offset) {
     return position;
   }
-  var clockPosition = this.getClockPositionFromPosition(position);
+  var clockPosition = this.getClockPositionFromPosition(position, beatsPerBar);
   var clockOffset = offset / 96;
   return this.getPositionFromClockPosition(clockPosition + clockOffset);
 }, function (position, offset) {
@@ -164,7 +168,7 @@ proto.emitStep = function emitStep (step) {
   var note = step.args;
   step.time = step.time + offset;
   step.clockPosition = step.position;
-  step.position = step.event === 'start' ? note.position : note.duration ?  this.getPositionWithOffset(note.position, note.duration) : note.position;
+  step.position = step.event === 'start' ? note.position : note.duration ?  this.getPositionWithOffsetAndBeatsPerBar(note.position, note.duration, this.beatsPerBar()) : note.position;
   if (step.event === 'stop'  && step.position === note.position) {
     return;
   }
@@ -194,7 +198,7 @@ proto.notesForSet = memoize(function notesForSet (id, notes, beatsPerBar, loopLe
         note = self.expandNote(note);
       }
       var normal = positionHelper.normalizeNote(note);
-      notes[index] = [self.getClockPositionFromPosition(normal.position), self.getDurationFromTicks(normal.duration || 0), null, null, normal];
+      notes[index] = [self.getClockPositionFromPosition(normal.position, beatsPerBar), self.getDurationFromTicks(normal.duration || 0), null, null, normal];
     }
     else {
       notes[index] = null;
@@ -297,7 +301,7 @@ proto.setPosition = function setPosition (position) {
     throw new Error('Invalid argument: position is not valid');
   }
   this._position = position;
-  this.clock.setPosition(this.getClockPositionFromPosition(position));
+  this.clock.setPosition(this.getClockPositionFromPosition(position, this.beatsPerBar()));
 };
 
 proto.tempo = function tempo () {
